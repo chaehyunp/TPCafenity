119abac953c6bd110d1b7b8808651c614084bd78
chhue <chhue96@gmail.com>
2023-03-31 14:01:26 +0900

    attach - HomeFragment/Adapter[Tab(TabListFragment,TabMapFragment)-ViewPager]

--- app/src/main/java/com/ch96/tpcafenity/adapters/ViewPagerHomeAdapter.kt
+++ app/src/main/java/com/ch96/tpcafenity/adapters/ViewPagerHomeAdapter.kt
@@ -1,18 +1,20 @@
 package com.ch96.tpcafenity.adapters
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.ch96.tpcafenity.fragments.HomeFragment
 import com.ch96.tpcafenity.fragments.TabListFragment
 import com.ch96.tpcafenity.fragments.TabMapFragment
 
-class ViewPagerHomeAdapter(fragment: HomeFragment) : FragmentStateAdapter(fragment) {
-    override fun getItemCount(): Int = 2
+class ViewPagerHomeAdapter(fragmentActivity:FragmentActivity):FragmentStateAdapter(fragmentActivity) {
+    override fun getItemCount():Int = 2
 
-    override fun createFragment(position: Int): Fragment {
+    override fun createFragment(position:Int):Fragment {
         return when (position) {
             0 -> TabListFragment()
             else -> TabMapFragment()
         }
     }
+
 }
--- app/src/main/java/com/ch96/tpcafenity/fragments/HomeFragment.kt
+++ app/src/main/java/com/ch96/tpcafenity/fragments/HomeFragment.kt
@@ -1,11 +1,11 @@
 package com.ch96.tpcafenity.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import androidx.fragment.app.Fragment
 import android.view.View
 import android.view.ViewGroup
-import com.ch96.tpcafenity.R
+import androidx.fragment.app.FragmentActivity
 import com.ch96.tpcafenity.adapters.ViewPagerHomeAdapter
 import com.ch96.tpcafenity.databinding.FragmentHomeBinding
 import com.google.android.material.tabs.TabLayout
@@ -14,45 +14,26 @@ import com.google.android.material.tabs.TabLayoutMediator
 
 class HomeFragment : Fragment() {
 
-    val binding:FragmentHomeBinding by lazy { FragmentHomeBinding.inflate(layoutInflater) }
+    private val binding:FragmentHomeBinding by lazy { FragmentHomeBinding.inflate(layoutInflater) }
+
+    private val tabTextList = listOf("리스트로 보기", "지도로 보기")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //네트워크에서 불러올때
+    }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        binding.pager.adapter = ViewPagerHomeAdapter(requireActivity())
 
-        //탭 설정
-        binding.layoutTab.addOnTabSelectedListener(object:TabLayout.OnTabSelectedListener{
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                //탭이 선택되었을때
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab?) {
-                //탭이 선택되지 않은 상태로 변경되었을때
-            }
-
-            override fun onTabReselected(tab: TabLayout.Tab?) {
-                //이미 선택된 탭이 다시 선택되었을때
-            }
-
-        })
-
-        //ViewPager에 아답터 연결
-        binding.pager.adapter = ViewPagerHomeAdapter(this)
-        
         TabLayoutMediator(binding.layoutTab, binding.pager) { tab, position ->
-            when(position) {
-                0 -> tab.text = "리스트로 보기"
-                else -> tab.text = "지도로 보기"
-            }
+            tab.text = tabTextList[position]
         }.attach()
-        
+        return binding.root
     }
-
 }
